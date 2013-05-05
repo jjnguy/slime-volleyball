1330547c44df05ee3639de8502deb113bc6928db
Justin Nelson <justin.john.nelson@gmail.com>
2013-05-05 18:56:28 -0500

    We have a bouncy ball!

--- SlimeVolleyball.js
+++ SlimeVolleyball.js
@@ -11,20 +11,22 @@ var SlimeGame = function (canvas) {
 
     var slime1 = new Slime(self, 'green');
     var slime2 = new Slime(self, 'red');
-
-    self.paint = function () {
-        paintSlime(slime1);
-    }
+    var ball = new Ball();
 
     self.advance = function(){
         applyInput();
 
+        //advanceSlime(slime2);
+        paintBackground();
+
         advanceSlime(slime1);
-        advanceSlime(slime2);
-        // console.log(slime1);
+
+        advanceBall(ball);
+        console.log(slime1);
+        console.log(ball);
     };
 
-    var X_SLOW_RATE = 2;
+    var X_SLOW_RATE = 4;
     var GRAVITY = -10;
     var MAX_X_VELOCITY = 20;
 
@@ -38,8 +40,47 @@ var SlimeGame = function (canvas) {
         }
     };
 
+    function paintBackground(){
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0,0, width, height);
+    }
+
+    function advanceBall(ball){
+        //eraseBall(ball);
+
+        ball.position.y += ball.velocity.y;
+        ball.velocity.y -= GRAVITY;
+        
+        if (ball.position.y > 0){
+            ball.velocity.y = -40;
+        }
+
+
+        paintBall(ball);
+    }
+
+    var BALL_RADIUS = 20;
+
+    function paintBall(ball){
+        var position = translateCoordinates(ball.position);
+
+        ctx.beginPath();
+        ctx.fillStyle = 'red';
+        ctx.arc(position.x, position.y, BALL_RADIUS, 0, Math.PI * 2, true);
+        ctx.fill();
+    }
+
+    function eraseBall(ball){
+        var position = translateCoordinates(ball.position);
+        
+        ctx.beginPath();
+        ctx.fillStyle = 'white';
+        ctx.arc(position.x, position.y, BALL_RADIUS + 1, 0, Math.PI * 2, true);
+        ctx.fill();
+    }
+
     function advanceSlime(slime){
-        eraseSlime(slime);
+        //eraseSlime(slime);
 
         slime.position.x += slime.velocity.x;
         slime.velocity.x -= slime.velocity.x == 0 ? 0: (slime.velocity.x / Math.abs(slime.velocity.x)) * X_SLOW_RATE;
@@ -71,7 +112,6 @@ var SlimeGame = function (canvas) {
         ctx.beginPath();
         ctx.fillStyle = slime.color;
         ctx.arc(btm.x, btm.y, slime.height, 0, Math.PI, true);
-        ctx.lineTo(btm.x, btm.y);
         ctx.fill();
     }
 
@@ -81,8 +121,8 @@ var SlimeGame = function (canvas) {
 
         ctx.beginPath();
         ctx.fillStyle = 'white';
-        ctx.arc(btm.x, btm.y, slime.height, 0, Math.PI, true);
-        ctx.lineTo(btm.x, btm.y);
+        ctx.lineWidth = 3;
+        ctx.arc(btm.x, btm.y, slime.height+1, 0, Math.PI, true);
         ctx.fill();
     }
 
@@ -157,5 +197,11 @@ var Slime = function (parent, color) {
 var Ball = function () {
     var self = this;
 
+    self.position = {x : 200, y: -200};
+    self.velocity = {x : 0,y : 0};
 
 }
+
+
+
+
